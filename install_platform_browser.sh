0c870a7ab9442750e4cc777dd7a0a93f0c184b18
nthomas <n.thomas@robotikdose.com>
2016-10-27 15:04:46 +0200

    init

---
+++ install_platform_browser.sh
@@ -0,0 +1,10 @@
+echo "----------------"
+echo "removing browser"
+echo "----------------"
+cordova platform rm browser
+echo "----------------"
+echo "creating browser"
+echo "----------------"
+cordova platform add browser
+echo "----------------"
+cordova platform ls
